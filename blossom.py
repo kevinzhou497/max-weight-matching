5d9469ac8c5f8eb06893022b90d72d0e366c295c
Kevin Zhou <kevinzhou497@gmail.com>
2023-01-11 23:47:31 -0500

    adding to blossom

--- blossom.py
+++ blossom.py
@@ -1,8 +1,10 @@
 import networkx as nx
+from itertools import combinations, repeat
 # G is a non empty, undirected graph, networkX graph
 def blossom_algorithm(G):
   class Blossom:
-    # init
+    # slots, can rewrite this
+    __slots__ = ["childs", "edges", "mybestedges"]
 
     def leaves(self):
       for t in self.children:
@@ -13,7 +15,63 @@ def blossom_algorithm(G):
 
   # get the nodes of graph G
   nodes = list(G)
+  maxWeight = 0
+
+  # are we assuming integers or not?
+  mate = {}
+
+  # label.get(b) = 
+  # None if b is unlabeled
+  # 1 if b is an S-blossom
+  # 2 if b is a T-blossom
+  # label of a vertex is the label of its top-level containing blossom
+  # if v is inside a T-blossom, label[v] is 2 iff v is reachable
+  # from an S-vertex outside the blossom
+  # labls assigned during a stage and reset during augmentation
+  label = {}
+
+  # labeledge[b] = (v,w) is the edge
+  # through which b obtained its label (for top level blossom b)
+  # labeledge[w] = (v,w) is an edge through which w is reachable
+  # from outside the blossom if w is a vertex inside a T-blossom and label[w] == 2
+  labeledge = {}
+
+  # vertBlossom[v] is the top-level blossom that v is in
+  vertBlossom = dict(zip(nodes, nodes))
+
+  # parents[b] is its parent blossom
+  # if it's top level, then parents[b] is None
+  parents = dict(zip(nodes, repeat(None)))
+
+  # bases[b] is b's base vertex
+  bases = dict(zip(nodes, nodes))
+
+  bestedge = {}
+
+  # dualvar[v] = 2*u(v) where u(v) is the v's variable in the dual optimization problem
+  # initially, u(v) = maxweight / 2
+  dualvar = dict(zip(nodes, repeat(maxWeight)))
+
+  blossomdual = {}
+  zeroSlack = {}
+
+  # newly discovered S-vertices
+  queue = []
+
+  # making a new blossom with base = base, through S-vertices v and w
+  def addBlossom(base, v, w):
+    bb = vertBlossom[base]
+    bv = vertBlossom[v]
+    bw = vertBlossom[w]
+
+    b = Blossom()
+    bases[b] = base
+    parents[b] = None 
+    parents[bb] = b 
+
+    
 
 
-  maxweight = 0
+
+
   
